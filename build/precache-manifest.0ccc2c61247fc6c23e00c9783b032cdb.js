4ac25e417b50717790b0e8262ff6e5e45c7e4782
Nwosu-Promise <promiseokechukwu79@gmail.com>
2021-04-13 22:56:44 +0100

    feat: server for heroku

---
+++ build/precache-manifest.0ccc2c61247fc6c23e00c9783b032cdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163a73bec2e1e4d5a3231fc722377d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617d4b48ea83b34e3fb5",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d90a75022da14321f431",
+    "url": "/static/css/main.821de975.chunk.css"
+  },
+  {
+    "revision": "617d4b48ea83b34e3fb5",
+    "url": "/static/js/2.f26c92cd.chunk.js"
+  },
+  {
+    "revision": "6bd8fd2b371d963fe89847d367b6d51b",
+    "url": "/static/js/2.f26c92cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90a75022da14321f431",
+    "url": "/static/js/main.97db6a5c.chunk.js"
+  },
+  {
+    "revision": "d4668020e5a5302076cd",
+    "url": "/static/js/runtime-main.a7ab52e7.js"
+  }
+]);
